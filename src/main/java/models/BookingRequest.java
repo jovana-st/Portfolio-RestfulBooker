ba7cc7bbaa1d853de3085e6170b20df15b9a85b5
jovana-st <jovanast52@gmail.com>
2025-07-31 19:51:59 +0200

    Update BookingRequest.java

--- src/main/java/models/BookingRequest.java
+++ src/main/java/models/BookingRequest.java
@@ -1,5 +1,7 @@
 package models;
+import lombok.Data;
 
+@Data
 public class BookingRequest {
 
     String firstname;
@@ -9,51 +11,4 @@ public class BookingRequest {
     BookingDates bookingDates;
     String additionalneeds;
 
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public int getTotalprice() {
-        return totalprice;
-    }
-
-    public void setTotalprice(int totalprice) {
-        this.totalprice = totalprice;
-    }
-
-    public boolean getDepositpaid() {
-        return depositpaid;
-    }
-
-    public void setDepositpaid(boolean depositpaid) {
-        this.depositpaid = depositpaid;
-    }
-
-    public BookingDates getBookingDates() {
-        return bookingDates;
-    }
-
-    public void setBookingDates(BookingDates bookingDates) {
-        this.bookingDates = bookingDates;
-    }
-
-    public String getAdditionalneeds() {
-        return additionalneeds;
-    }
-
-    public void setAdditionalneeds(String additionalneeds) {
-        this.additionalneeds = additionalneeds;
-    }
 }
