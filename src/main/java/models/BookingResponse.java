aa06259d656c1ec91dba6381983cdb7cbc337fc6
jovana-st <jovanast52@gmail.com>
2025-07-31 19:52:15 +0200

    Update BookingResponse.java

--- src/main/java/models/BookingResponse.java
+++ src/main/java/models/BookingResponse.java
@@ -1,14 +1,9 @@
 package models;
+import lombok.Data;
 
+@Data
 public class BookingResponse {
 
     int bookingid;
 
-    public int getBookingid() {
-        return bookingid;
-    }
-
-    public void setBookingid(int bookingid) {
-        this.bookingid = bookingid;
-    }
 }
