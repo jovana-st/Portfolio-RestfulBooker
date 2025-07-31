76aa61804b175cca62e9da9ebd75f1622d7c675e
jovana-st <jovanast52@gmail.com>
2025-07-31 19:54:15 +0200

    Update ApiAssertions.java

--- src/main/java/utils/ApiAssertions.java
+++ src/main/java/utils/ApiAssertions.java
@@ -2,6 +2,10 @@ package utils;
 
 import io.restassured.response.Response;
 import org.testng.Assert;
+import java.io.File;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
+
 
 public class ApiAssertions {
 
@@ -22,13 +26,22 @@ public class ApiAssertions {
 
     //Assert that the response time is less than a specific amount
     public static void assertResponseTimeLessThan(Response response, long maxTime){
-        long responseTime = response.getTime();
-        Assert.assertTrue(responseTime < maxTime);
+        Assert.assertTrue(response.getTime() < maxTime);
     }
 
     //Assert that the response body is empty
     public static void assertEmptyBody(Response response){
         Assert.assertTrue(response.getBody().asString().isEmpty());
+    }
+
+    //Assert that the header exists
+    public static void assertHeaderExists(Response response, String headerName){
+        Assert.assertTrue(response.getHeaders().hasHeaderWithName(headerName));
+    }
+
+    //Assert JSON Schema
+    public static void assertJsonSchema(Response response, String schemaPath){
+        response.then().assertThat().body(matchesJsonSchema(new File(schemaPath)));
     }
 
 }
