4618eff8ae39ed37cda82f316324f757ed443cb1
jovana-st <jovanast52@gmail.com>
2025-07-31 19:51:09 +0200

    Update AuthRequest.java

--- src/main/java/models/AuthRequest.java
+++ src/main/java/models/AuthRequest.java
@@ -1,20 +1,12 @@
 package models;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class AuthRequest {
 
     String username;
     String password;
 
-    public AuthRequest(String username, String password){
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
