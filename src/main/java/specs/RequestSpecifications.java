553096cf5c2e3e39748666ff39e70b148ba31ec6
jovana-st <jovanast52@gmail.com>
2025-07-31 19:53:54 +0200

    Update RequestSpecifications.java

--- src/main/java/specs/RequestSpecifications.java
+++ src/main/java/specs/RequestSpecifications.java
@@ -1,4 +1,4 @@
-package core;
+package specs;
 
 import config.Constants;
 import io.restassured.builder.RequestSpecBuilder;
@@ -16,14 +16,18 @@ public class RequestSpecifications {
     public static RequestSpecification baseSpecCreateBooking = new RequestSpecBuilder().setBaseUri(Constants.BASE_URL)
             .addHeader("Content-Type", "application/json").build();
 
-    public static RequestSpecification baseSpecUpdateBooking = new RequestSpecBuilder().setBaseUri(Constants.BASE_URL)
-            .addHeader("Content-Type", "application/json")
-            .addHeader("Acccept", "application/json")
-            .addHeader("Cookie", "token=" + AuthService.getAuthToken())
-            .build();
+    public static RequestSpecification baseSpecUpdateBooking(){
+        return new RequestSpecBuilder().setBaseUri(Constants.BASE_URL)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Acccept", "application/json")
+                .addHeader("Cookie", "token=" + AuthService.getAuthToken())
+                .build();
+    }
 
-    public static RequestSpecification baseSpecDeleteBooking = new RequestSpecBuilder().setBaseUri(Constants.BASE_URL)
-            .addHeader("Content-Type", "application/json")
-            .addHeader("Cookie", "token=" + AuthService.getAuthToken())
-            .build();
+    public static RequestSpecification baseSpecDeleteBooking(){
+        return new RequestSpecBuilder().setBaseUri(Constants.BASE_URL)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Cookie", "token=" + AuthService.getAuthToken())
+                .build();
+    }
 }
