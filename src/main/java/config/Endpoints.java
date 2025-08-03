a534cc9830c19091d23f183d0ca8bf887147f22d
jovana-st <jovanast52@gmail.com>
2025-08-03 12:49:40 +0200

    Update Endpoints.java

--- src/main/java/config/Endpoints.java
+++ src/main/java/config/Endpoints.java
@@ -4,6 +4,7 @@ public class Endpoints {
 
     public static final String BOOKING = "/booking";
     public static final String AUTH = "/auth";
-    public final static String PING = "/ping";
+    public static final String PING = "/ping";
+
 
 }
